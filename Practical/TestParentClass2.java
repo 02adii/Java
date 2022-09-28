7891f553b40adacaae2ba60036470a72e3f98f8c
02adii <114074096+02adii@users.noreply.github.com>
2022-09-28 17:53:06 +0530

    Add files via upload

---
+++ Practical/TestParentClass2.java
@@ -0,0 +1,15 @@
+package CoreJava;
+
+ class TestParentClass2 {
+
+	public static void main(String[] args) {
+		//creating object 
+		ParentClass2 p = new ParentClass2();
+		//calling the method using object
+		p.text();
+		p.text1();
+		
+
+	}
+
+}
