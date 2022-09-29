762864e4a71ae488cb768a91830ac5a8b4fef47f
02adii <114074096+02adii@users.noreply.github.com>
2022-09-29 17:01:22 +0530

    Add files via upload

---
+++ Lab Sessions/29-09-2022/Child.java
@@ -0,0 +1,14 @@
+// this class inherits the parent class 
+package CoreJava;
+
+//class Child inheriting class Parent 
+class Child extends Parent
+{   public void speak()
+	 {
+		 System.out.println("My name is Aditya Kumar but my friends like to  call me Adii. ");
+	 }
+	public void dance()
+	 {
+		 System.out.println("Sorry!, I can't dance.");
+	 }
+}
